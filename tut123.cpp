5422a9db9013dd235059e3e2528d7e01f9ce1b34
Snehasish Bhattacharjee <snehasishbhattacharjee2000@gmail.com>
2023-07-15 20:53:47 +0530

    New Test

---
+++ tut123.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class number
+{
+    int a;
+
+public:
+    number()
+    {
+        a = 0;
+    }
+    number(int num)
+    {
+        a = num;
+    }
+    number(number &obj)
+    {
+        cout << "Copy constructor called" << endl;
+        a = obj.a;
+    }
+
+    void display()
+    {
+        cout << "The number for this object is " << a << endl;
+    }
+};
+
+int main()
+{
+    number x, y, z(45);
+    x.display();
+    y.display();
+    z.display();
+    number z1(x);
+    z1.display();
+
+    return 0;
+}
