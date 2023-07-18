5fdbe72d09c82cdfce0e52e713ddc5cc51d3aba0
Snehasish Bhattacharjee <snehasishbhattacharjee2000@gmail.com>
2023-07-18 21:04:26 +0530

    c test

---
+++ tutc.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+    printf("Hello world\n");
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d", &b);
+    printf("%d\n", a);
+    printf("%d", b);
+    return 0;
+}
